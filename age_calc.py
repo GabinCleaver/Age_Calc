78dee8320d73c2879bfabc52288218e71a5ad0d4
MrGabin <79531012+GabinCleaver@users.noreply.github.com>
2021-06-06 23:08:04 +0200

    Update age_calc.py

--- age_calc.py
+++ age_calc.py
@@ -1,4 +1,23 @@
-birth_year = int(input('Entrez votre année de naissance: '))
-year = int(input("Entrez l'année actuelle: "))
-age = year - birth_year
-print("Vous avez :", age, "ans")
+choice = int(input("""Entrez la valeur
+1. Age Calcul Simple
+2. Calcul de deux Age + Comparaison
+
+[>] """))
+
+if choice == 1:
+    year = int(input("Entrez l'année actuelle: "))
+    birth_year = int(input("Entrez votre année de naissance: "))
+    age = year - birth_year
+    print("Vous avez :", age, "ans")
+elif choice == 2:
+    year = int(input("Entrez l'année actuelle: "))
+    birth_year = int(input("Entrez votre année de naissance: "))
+    second_birth_year = int(input("Entrez l'année de naissance de l'autre personne: "))
+    age = year - birth_year
+    age_other = year - second_birth_year
+    print(f"""Vous avez : {age} ans
+    Il a : {age_other} ans""")
+    age_comparaison = age - age_other
+    print("Vous avez " + str(age_comparaison) + " ans d'écart.")
+else:
+    print("Erreur")
